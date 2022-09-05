15499582abd4a998371f59aae492f46bb6ed7e90
Eliseo Martelli <me@eliseomartelli.it>
2022-09-05 17:30:21 +0200

    chore: bump version

--- Casks/muteunmute.rb
+++ Casks/muteunmute.rb
@@ -1,6 +1,6 @@
 cask "muteunmute" do
-  version "1.4.3"
-  sha256 "b5db34ca1f94365358b8790e67fd1c9f88a9e2ed4e4266149b49829f36866dad"
+  version "1.4.3b"
+  sha256 "115ba4896067eace882170ce168ee083257413424d54c54cc5f21e88634b7610"
   url "https://github.com/eliseomartelli/MuteUnmuteMic/releases/download/#{version}/release.zip"
 
   name "MuteUnmuteMic"
