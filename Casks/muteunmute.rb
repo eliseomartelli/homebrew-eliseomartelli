13eabf9f31c5968623bad0ccc56d9e09cce93d8e
Eliseo Martelli <me@eliseomartelli.it>
2022-09-05 16:31:46 +0200

    fix: add description to cask

--- Casks/muteunmute.rb
+++ Casks/muteunmute.rb
@@ -2,7 +2,9 @@ cask "muteunmute" do
   version "1.4.3"
   sha256 "b5db34ca1f94365358b8790e67fd1c9f88a9e2ed4e4266149b49829f36866dad"
   url "https://github.com/eliseomartelli/MuteUnmuteMic/releases/download/#{version}/release.zip"
+
   name "MuteUnmuteMic"
+  desc "Mute and unmute microphone menubar app"
 
   app "[Un]MuteMic.app"
 end
