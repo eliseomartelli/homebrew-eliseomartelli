d2133c34cfbafcfc5dd72510ae8a9d2021aa6308
Eliseo Martelli <me@eliseomartelli.it>
2022-09-06 15:49:08 +0200

    chore: update muteunmute

--- Casks/muteunmute.rb
+++ Casks/muteunmute.rb
@@ -1,8 +1,8 @@
 cask "muteunmute" do
-  version "1.4.3b"
-  sha256 "115ba4896067eace882170ce168ee083257413424d54c54cc5f21e88634b7610"
-  url "https://github.com/eliseomartelli/MuteUnmuteMic/releases/download/#{version}/release.zip"
+  version "1.4.3c"
+  sha256 "5df622b4f8f72b61414199c570a3192919beba859f23429c36a8b811f8b92139"
 
+  url "https://github.com/eliseomartelli/MuteUnmuteMic/releases/download/#{version}/release.zip"
   name "MuteUnmuteMic"
   desc "Mute and unmute microphone menubar app"
 
