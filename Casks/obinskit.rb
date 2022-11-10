6000d070317288ee50f60f2f236d3e3559a62744
Eliseo Martelli <me@eliseomartelli.it>
2022-11-10 15:42:58 +0100

    feat: add obinskit

---
+++ Casks/obinskit.rb
@@ -0,0 +1,10 @@
+cask "obinskit" do
+  version "1.2.11"
+  sha256 "751676278cf6919059db67985572ff37b0515fc4937ab33aeae6987b7fb5c371"
+  url "https://s3.hexcore.xyz/occ/darwin/x64/ObinsKit_#{version}_x64.dmg"
+
+  name "ObinsKit"
+  desc "Manage AnnePro Keyboards"
+
+  app "ObinsKit.app"
+end
