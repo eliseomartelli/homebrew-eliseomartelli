9be097aaebc152edabd86d7a2c1daf42a6151072
Eliseo Martelli <me@eliseomartelli.it>
2024-03-30 23:24:46 +0100

    chore: update calendr

--- Casks/calendr.rb
+++ Casks/calendr.rb
@@ -1,6 +1,6 @@
 cask "calendr" do
-  version "v1.10.8"
-  sha256 "f4f2983a132f6d27957cb7149b3319ca6f5d810ba868b89258c3defce0f0345e"
+  version "v1.10.9"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 
   url "https://github.com/pakerwreah/Calendr/releases/download/#{version}/Calendr.zip"
   name "Calendr"
